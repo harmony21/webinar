ecc37771e0e00c709b8910397811ecb189ace9b4
Inna Botvinnik <21icelady97@gmail.com>
2018-01-16 21:07:50 +1000

    add 4 more sections, changed 1 and 4

---
+++ js/onepage-scroll.js
@@ -0,0 +1,15 @@
+$(".main").onepage_scroll ({
+   sectionContainer: "section",     
+   easing: "ease",                                                  
+   animationTime: 1000,             
+   pagination: true,                
+   updateURL: false,                
+   beforeMove: function(index) {},  
+   afterMove: function(index) {},   
+   loop: false,                     
+   keyboard: true,                  
+   responsiveFallback: false,                                                   
+   direction: "vertical"              
+});
+
+
