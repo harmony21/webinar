896de19be2991c07badfe622928e2bf94f7df77e
Inna Botvinnik <21icelady97@gmail.com>
2018-01-19 17:17:59 +1000

    add mailerlite form

---
+++ js/mailerlite.js
@@ -0,0 +1,6 @@
+function ml_webform_success_7421100() {
+                                var $ = ml_jQuery || jQuery;
+
+                                $('.ml-subscribe-form-7421100 .ml-block-success').show();
+                                $('.ml-subscribe-form-7421100 .ml-block-form').hide();
+                            };
